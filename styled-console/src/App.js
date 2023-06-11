09334b7c21a0f16d77d24086daa346a9059e72e8
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-06-11 17:09:51 +0300

    edited global styles

--- styled-console/src/App.js
+++ styled-console/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const AppWrapper  = styled.div`
+width: 100%;
+min-height: 100vh;
+padding: 2rem;
+background: black;
+
+`
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <AppWrapper>
+asdasdasdasd
+    </AppWrapper>
   );
 };
 
